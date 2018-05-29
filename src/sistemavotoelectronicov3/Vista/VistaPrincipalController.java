6545f845a1ee095ada4924c685a357ce4b59f5da
Tomás Contreras <Toms Contreras@10.10.6.88>
2018-05-29 15:46:00 -0300

    CreandoJavaFx con SceneBuilder

---
+++ src/sistemavotoelectronicov3/Vista/VistaPrincipalController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemavotoelectronicov3.Vista;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author Tomás Contreras
+ */
+public class VistaPrincipalController implements Initializable {
+
+    @FXML
+    private Button primerboton;
+    @FXML
+    private ComboBox<?> primercombobox;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
